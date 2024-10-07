114187eebd536ea8f87bbdc0429743c4da863cb4
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-08 01:06:41 +0900

    Edited `vite.config.ts`.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'SPATestOnGitHubPages',
+    root: 'src',
+    base: '/SPATestOnGitHubPages/',
+    build: {
+        outDir: '../docs'
+    },
     clearScreen: false,
     server: {
+        port: 8080,
         watch: {
             ignored: [
                 "**/*.fs" // Don't watch F# files
