c29371470182e2ccf8d5618729515bec89433655
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-07 23:41:23 +0900

    `dotnet fable` after cd .\src\

--- init.ps1
+++ init.ps1
@@ -15,3 +15,4 @@ New-Item -Name vite.config.ts -ItemType File
 # Add `base: '<REPO>',` to vite.config.ts.
 New-Item -Name index.html -ItemType File
 New-Item -Name style.css -ItemType File
+# `dotnet fable` after cd .\src\
---
+++ src/Program.fs.js
@@ -0,0 +1,4 @@
+import { printf, toConsole } from "./fable_modules/fable-library-js.4.22.0/String.js";
+
+toConsole(printf("Hello from F#"));
+
