937d17b6fd7ad9d96d8c804b6b9d83bbd5ebacee
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-07 22:50:02 +0900

    `dotnet new tool-manifest`

--- init.ps1
+++ init.ps1
@@ -1,3 +1,4 @@
 dotnet new console --language "F#" --name "App" --framework "net8.0" --output src
 dotnet new globaljson --sdk-version "8.0.100" --roll-forward latestFeature
 dotnet new nugetconfig
+dotnet new tool-manifest
