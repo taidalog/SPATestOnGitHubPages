3eed4fdcda1bd000e5ae83d332149d8e974c8dfc
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-09 23:36:27 +0900

    Introduced `window.history.pushState`.

--- src/Program.fs
+++ src/Program.fs
@@ -1,5 +1,24 @@
 ﻿namespace SPATestOnGitHubPages
 
+open Fable.Core
+open Fable.Core.JsInterop
+open Browser.Dom
+open Browser.Types
+
 module Program =
     A.f ()
     A.g ()
+
+    window.addEventListener (
+        "DOMContentLoaded",
+        (fun _ ->
+            document.body.getElementsByTagName "a"
+            |> (fun x -> JS.Constructors.Array?from(x))
+            |> Array.iter (fun (x: HTMLAnchorElement) ->
+                x.onclick <-
+                    fun e ->
+                        e.preventDefault ()
+                        window.history.pushState (null, "", x.pathname)))
+    )
+
+    window.addEventListener ("popstate", (fun _ -> printfn "%s" window.location.pathname))
--- src/Program.fs.js
+++ src/Program.fs.js
@@ -1,6 +1,25 @@
 import { g, f } from "./A.fs.js";
+import { defaultOf } from "./fable_modules/fable-library-js.4.22.0/Util.js";
+import { printf, toConsole } from "./fable_modules/fable-library-js.4.22.0/String.js";
 
 f(undefined);
 
 g(undefined);
+
+window.addEventListener("DOMContentLoaded", (_arg) => {
+    let array;
+    const x = document.body.getElementsByTagName("a");
+    array = (Array.from(x));
+    array.forEach((x_1) => {
+        x_1.onclick = ((e) => {
+            e.preventDefault();
+            window.history.pushState(defaultOf(), "", x_1.pathname);
+        });
+    });
+});
+
+window.addEventListener("popstate", (_arg) => {
+    const arg = window.location.pathname;
+    toConsole(printf("%s"))(arg);
+});
 
